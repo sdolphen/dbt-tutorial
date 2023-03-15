8dfafe4213f9df30957ee907bee84ae2280d3afc
Stijn Dolphen <stijn@dataroots.io>
2023-03-15 12:54:39 +0000

    dbt training

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -3,7 +3,7 @@ with customers as (
     select * from {{ ref('stg_customers')}}
 ),
 
-with orders as (
+orders as (
 
     select * from {{ ref('stg_orders')}}
 ),
--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -1,7 +1,7 @@
-with orders as(
+with orders as (
     select
-        id as order_id
-        user_id as customer_id
+        id as order_id,
+        user_id as customer_id,
         order_date,
         status
     from raw.jaffle_shop.orders
