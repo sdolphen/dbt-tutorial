0b1b49d710d7a3107bc5b5f4183f5ef4f16d8026
Stijn Dolphen <stijn@dataroots.io>
2023-05-05 09:22:30 +0000

    qsd

--- models/jinja-training/macros_jinja.sql
+++
@@ -1,6 +0,0 @@
-{% macro cents_to_dollars(column_name, decimal_places) %}
-
-round( {{column_name}}/100, {{ decimal_places }} )
-
-{% endmacro %}
-
--- models/jinja-training/stg_payments.sql
+++
@@ -1,9 +0,0 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    -- amount stored in cents, convert to dollars
-    {{ cents_to_dollars(‘payment_amount’) }} as amount
-    created as created_at
-from {{ source(‘stripe’, ‘payment’) }}
